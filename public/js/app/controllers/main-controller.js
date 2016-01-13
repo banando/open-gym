e8c52c7f3bb032e4e570c184c0af6a06a304a1ae
Fernando Orozco <forozco2085@gmail.com>
2016-01-12 19:32:19 -0800

    remove redundant google-maps call, add markers, yelp api call

--- public/js/app/controllers/main-controller.js
+++ public/js/app/controllers/main-controller.js
@@ -6,9 +6,9 @@
     .module('app')
     .controller("MainController", MainController);
 
-  MainController.$inject = ['$state', 'userDataService', '$log', "authService", "tokenService", "uiGmapGoogleMapApi"];
+  MainController.$inject = ['$state', 'userDataService', '$log', "authService", "tokenService", "uiGmapGoogleMapApi", "$http"];
 
-  function MainController($state, userDataService, $log, authService, tokenService, uiGmapGoogleMapApi){
+  function MainController($state, userDataService, $log, authService, tokenService, uiGmapGoogleMapApi, $http){
     var vm = this;
 
     vm.user   = userDataService;
@@ -20,8 +20,24 @@
         latitude: 34,
         longitude: -118
       },
-      zoom: 6
+      zoom: 8
     };
+    vm.marker = {
+      windowOptions: {
+        visible: true
+      },
+      title:  "You are here.",
+      id:     "1"
+    };
+
+    vm.onClick = function() {
+      console.log(vm.marker);
+      vm.marker.windowOptions.visible = !vm.marker.windowOptions.visible;
+    };
+    vm.closeClick = function () {
+      vm.marker.windowOptions.visible = false;
+    };
+
 
     function logOut() {
       $log.debug("logging out: ", vm.user.name);
@@ -31,23 +47,59 @@
       vm.user.email   = "";
       vm.user.password= "";
     }
+
+    function randomString(length){
+      var result = "";
+      var possible = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+      for (var i = 0; i < length; i++) {
+        result += possible.charAt(Math.floor(Math.random() * possible.length));
+      };
+      return result;
+    }
+
     var map;
-    var geocoder = new google.maps.Geocoder;
     if(navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
         console.log(position);
         vm.map = {
           center: {latitude: position.coords.latitude, longitude: position.coords.longitude},
-          zoom: 10
+          zoom: 12
           };
+        vm.marker.coords = {latitude: position.coords.latitude, longitude: position.coords.latitude}
+        yelpCall(position);
+      });
+
+    };
+    function yelpCall (position) {
+      var method = "GET";
+      var url    = "http://api.yelp.com/v2/search";
+      var params = {
+        callback: 'angular.callbacks._0',
+        ll: position.coords.latitude + ',' + position.coords.longitude,
+        oauth_consumer_key: "6a42RgFrThDODyLQxWbNuA",
+        oauth_token: "zMDrkoyu_3Gwt4FAjopisZpVG2Z-YV6w",
+        oauth_signature_method: "HMAC-SHA1",
+        oauth_timestamp: new Date().getTime(),
+        oauth_nonce: randomString(32),
+        category_filter: "basketballcourts",
+      };
+      var consumerSecret = "1WDCxcRCfi-q_WwcpvZTDYBJ9yU";
+      var tokenSecret = "BsleiqAMY-IVbsM0AhjA83P549A";
+      var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, {encodeSignature: false});
+      params['oauth_signature'] = signature;
+      $http.jsonp(url, {params: params}).success(function(data){
+          vm.courts = data.buisnesses;
+          console.log(data);
       });
     };
 
 
 
 
-    uiGmapGoogleMapApi.then(function(maps) {
 
+    uiGmapGoogleMapApi.then(function(maps) {
+      vm.maps = maps;
+      console.log(maps);
     });
 
   }
